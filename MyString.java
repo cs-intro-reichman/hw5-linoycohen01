27dd335ba63c42fef243bf6daf0f991371d16514
linoycohen01 <linoy.cohen01@post.runi.ac.il>
2024-12-07 23:21:28 +0200

    1

--- MyString.java
+++ MyString.java
@@ -8,8 +8,20 @@ public class MyString {
         System.out.println(countChar(hello, 'l'));
         System.out.println(countChar(hello, 'z'));
         System.out.println(spacedString(hello));
-        //// Put your other tests here.
+        System.out.println(remove("committee", "meet"));
+        System.out.println("\nTesting remove:");
+        System.out.println("committee - meet -> " + MyString.remove("committee", "meet") + " (expected: comit)");
+        System.out.println("abc - abc -> " + MyString.remove("abc", "abc") + " (expected: )");
+        System.out.println("abc - b -> " + MyString.remove("abc", "b") + " (expected: ac)");
+        System.out.println("hello - empty string -> " + MyString.remove("hello", "") + " (expected: hello)");
     }
+ 
+    
+    
+    
+
+        //// Put your other tests here.
+    
 
     /**
      * Returns the number of times the given character appears in the given string.
@@ -20,8 +32,16 @@ public class MyString {
      * @return the number of times c appears in str
      */
     public static int countChar(String str, char ch) {
-        //// Replace the following statement with your code
-        return 0;
+        int count = 0;
+        if(str.isEmpty()){
+            return 0;
+        }
+        for (int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == ch){
+                count++;
+            }
+        }
+        return count;
     }
 
     /** Returns true if str1 is a subset string str2, false otherwise
@@ -36,8 +56,15 @@ public class MyString {
      * @return true is str1 is a subset of str2, false otherwise
      */
     public static boolean subsetOf(String str1, String str2) {
-         //// Replace the following statement with your code
-        return false;
+        if(str1.isEmpty()){
+            return true;
+        }
+        for (int i = 0; i < str1.length(); i++){
+            if(countChar(str2, str1.charAt(i)) != countChar(str1,  str1.charAt(i))){
+                return false;
+            }
+         }
+        return true;
     }
 
     /** Returns a string which is the same as the given string, with a space
@@ -49,8 +76,15 @@ public class MyString {
      * @return a string consisting of the characters of str, separated by spaces.
      */
     public static String spacedString(String str) {
-        //// Replace the following statement with your code
-        return null;
+        String spaced = "";
+        if(str.isEmpty()){
+            return " ";
+        }
+        for (int i = 0; i < str.length()-1; i++){
+            spaced += str.charAt(i) + " ";
+        }
+        spaced += str.charAt(str.length()-1);
+        return spaced;
     }
   
     /**
@@ -64,8 +98,16 @@ public class MyString {
      * @return a randomly generated string, consisting of 'n' lowercase letters
      */
     public static String randomStringOfLetters(int n) {
-        //// Replace the following statement with your code
-        return null;
+        if (n == 0){
+            return "";
+        }
+        String random = "";
+        for (int i = 0; i < n; i++){
+            int ran = (int)(Math.random() * (122 - 97 + 1) + 97);
+            random += (char)(ran);
+        }
+
+        return random;
     }
 
     /**
@@ -77,10 +119,16 @@ public class MyString {
      * @param str2 - a string
      * @return a string consisting of str1 minus all the characters of str2
      */
-    public static String remove(String str1, String str2) {
-       //// Replace the following statement with your code
-        return null;
+    
+public static String remove(String str1, String str2) {
+    for (int i = 0; i < str2.length(); i++) {
+        char currentChar = str2.charAt(i);
+        int currentIndex = str1.indexOf(currentChar);
+        str1 = str1.substring(0, currentIndex) + str1.substring(currentIndex + 1);        
     }
+    return str1;
+}
+
 
     /**
      * Returns a string consisting of the given string, with the given 
